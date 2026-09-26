db1048fdf24178bf7f23e2b7bbe8020bebd7d9b4
metagliatore <metagliatore@users.noreply.github.com>
2026-09-26 11:30:19 +0200

    fix(docker): clean stale Xvfb lock files on container restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Pulisci eventuali lock file residui di Xvfb da riavvii precedenti
+rm -f /tmp/.X99-lock /tmp/.X11-unix/X99
+
 # Avvia Xvfb (display virtuale) in background per permettere a Chrome di girare
 # anche se la sessione richiede una finestra visibile (es. durante il login)
 Xvfb :99 -screen 0 1280x1024x24 -ac &
